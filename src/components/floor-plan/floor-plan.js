4242fb582a477270715962b3244f762db8efae1b
Ivan HA <hangolam@gmail.com>
2017-03-14 11:37:35 +0800

    fix issue with structure changes

--- src/components/floor-plan/floor-plan.js
+++ src/components/floor-plan/floor-plan.js
@@ -4,12 +4,15 @@ import './floor-plan.css';
 // <seat v-for='col in row.columns'></seat>
 Vue.component('floor-plan', {
   template: `
-              <div class='floor-plan'>
-                <div class='screen' v-if='plan.screenPos === 'top''></div>
-                <div class='row' v-for='row in plan.rows'>
-                  <span class='row-name'>{{row.name}}</span>
+              <div class="floor-plan">
+                <div class="screen" v-if="plan.screenPos === 'top'"><span>Screen</span></div>
+                <div class="floor-plan__row" v-for="row in plan.rows">
+                  <span class="row-name">{{row.name}}</span>
+                  <span v-for="col in row.columns">
+                    <seat :seat="col"></seat>
+                  </span>
                 </div>
-                <div class='screen' v-if='plan.screenPos === 'bottom''></div>
+                <div class="screen" v-if="plan.screenPos === 'bottom'"><span>Screen</span></div>
               </div>
             `,
   props: {
