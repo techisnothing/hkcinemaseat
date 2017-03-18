d3d4a61c77810dd8eba66fd2dbc151b43babbae0
samuel <kwokyinlun@gmail.com>
2017-03-18 13:13:25 +0800

    [Feature] components: add history management to house tab

--- src/components/tab/tab.js
+++ src/components/tab/tab.js
@@ -1,50 +1,23 @@
 //@flow
 import Vue from 'vue';
-import _ from 'lodash';
 import template from './tab.html';
-
-
-function transfrom_list(house){
-	return _.map(house, (el)=>{
-		//TODO: should have a more robust api for house list
-		let house_id = el.split(' ')[2];
-		return {
-			id: house_id,
-			name: `House ${house_id}`,
-			url: `/broadway/mk/${house_id}`
-		};
-	});
-}
 
 Vue.component('tab', {
 	template,
 	props:{
-		houselist: {
+		houses: {
 			type: Array
+		},
+		currenthouse:{
+			type: String
 		}
-	},
-	computed:{
-		houses(){
-			return transfrom_list(this.houselist);
-		}
-	},
-	data(){
-		return {
-			active: null,
-		};
 	},
 	methods:{
 		onClick(id){
-			this.active = id;
 			this.$emit('floorplan-change', id);
 		},
 		isActive(id){
-			return this.active === id;
-		}
-	},
-	watch: {
-		houselist(){
-			this.active = _.get(this.houses, '[0].id');
+			return this.currenthouse === id;
 		}
 	}
 });
--- src/details.js
+++ src/details.js
@@ -1,26 +1,41 @@
 import Vue from 'vue';
 import vue_http from 'vue-resource';
+import _ from 'lodash';
+import historyManager from './manager/historyManager';
 import './common/clear.css';
 import './common/cinemalist.css';
 import './components/house-info/house-info';
 import './components/seat/seat';
 
-import cinemaList from '../backend/data/cinema.json';
-
 const CINEMA_DETAILS_REGEX = /^\/([a-z]*)\/([a-z]*)/;
+
+const extract_housename = (path) => {
+	return _.last(path.split('/'));
+};
 
 Vue.use(vue_http);
 
 new Vue({
 	el: '#app',
 	created(){
+		let house = extract_housename(window.location.pathname);
 		this.fetch_house_list();
-		this.fetch_seat_plan();
+		this.fetch_seat_plan('broadway', 'mk', house)
+			.then(()=>{
+				historyManager.replaceState({house}, null , house);
+			});
+
+
+		historyManager.on('history_change',(state)=>{
+			let {house}  = state;
+			this.fetch_seat_plan('broadway', 'mk', house);
+		});
 	},
 	data: {
 		header: '揀位王',
 		plan: {},
 		houselist: [],
+		currenthouse: '',
 	},
 	computed: {
 		cinemaName: function() {
@@ -30,6 +45,17 @@ new Vue({
 			} else {
 				return '';
 			}
+		},
+		houses(){
+			return _.map(this.houselist, (el)=>{
+				//TODO: should have a more robust api for house list
+				let house_id = el.split(' ')[2];
+				return {
+					id: house_id,
+					name: `House ${house_id}`,
+					url: `/broadway/mk/${house_id}`
+				};
+			});
 		}
 	},
 	methods:{
@@ -39,11 +65,16 @@ new Vue({
 				this.houselist = list;
 			});
 		},
+		on_floor_plan_change(brand= 'broadway', venue = 'mk', house = '1'){
+			this.fetch_seat_plan(brand, venue, house).then(()=>{
+				historyManager.pushState({house}, null, house);
+			});
+		},
 		fetch_seat_plan(brand= 'broadway', venue = 'mk', house = '1'){
-
 			let dist_url = `/api/cinema/${brand}/${venue}/${house}`;
-			this.$http.get(dist_url).then(({body: plan})=>{
+			return this.$http.get(dist_url).then(({body: plan})=>{
 				this.plan = plan;
+				this.currenthouse = house;
 			});
 		}
 	}
